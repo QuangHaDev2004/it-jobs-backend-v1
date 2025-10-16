165835f84a04a32302e6b428367de7cb1771138a
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-10-16 16:00:37 +0700

    feat: register

--- index.ts
+++ index.ts
@@ -1,11 +1,30 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import routes from "./routes/index.route";
+import { connectDB } from "./config/database.config";
 
 const app = express();
 const port = 8080;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("OK");
-});
+// Load biến môi trường từ file env
+dotenv.config();
+
+// Kết nối CSDL
+connectDB();
+
+// Cấu hình CORS
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+// Cho phép gửi dữ liệu lên dạng JSON
+app.use(express.json());
+
+// Thiết lập đường dẫn
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Website đang chạy trên cổng ${port}`);
