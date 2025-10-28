1ddbb587f2437e573c1f454ea866dbe35576272e
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-10-28 20:18:37 +0700

    feat: pagination

--- controllers/company.controller.ts
+++ controllers/company.controller.ts
@@ -138,11 +138,26 @@ export const listJob = async (req: AccountRequest, res: Response) => {
   try {
     const companyId = req.account.id;
 
-    const jobs = await Job.find({
+    const find = {
       companyId: companyId,
-    }).sort({
-      createdAt: "desc",
-    });
+    };
+    
+    // Phân trang
+    const limitItems = 2;
+    let page = 1;
+    if (req.query.page && parseInt(req.query.page as string) > 0) {
+      page = parseInt(req.query.page as string);
+    }
+    const skip = (page - 1) * limitItems;
+    const totalRecord = await Job.countDocuments(find);
+    const totalPage = Math.ceil(totalRecord / limitItems);
+
+    const jobs = await Job.find(find)
+      .sort({
+        createdAt: "desc",
+      })
+      .limit(limitItems)
+      .skip(skip);
 
     const dataFinal = [];
 
@@ -165,6 +180,7 @@ export const listJob = async (req: AccountRequest, res: Response) => {
       code: "success",
       message: "Thành công!",
       jobs: dataFinal,
+      totalPage: totalPage
     });
   } catch (error) {
     res.json({
